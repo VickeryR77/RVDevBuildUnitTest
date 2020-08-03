ec7d1a6ef810c8ca1268078fd8c12a5b6e7e59a4
VickeryR77 <67699299+VickeryR77@users.noreply.github.com>
2020-08-03 18:54:19 -0400

    Add files via upload

---
+++ PrimeNumbersTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace PrimeLab.Tests
+{
+    public class PrimeNumbersTest
+    {
+
+        [Fact]
+        public void Test1()
+        {
+            int choice = 1;
+            ArrayList myList = new ArrayList();
+            PrimeNumbers.CreatePrimeList(myList);
+            Assert.Equal(2, PrimeNumbers.GetPrime(myList, choice));
+
+        }
+        [Fact]
+        public void Test2()
+        {
+            int choice = 2;
+            ArrayList myList = new ArrayList();
+            PrimeNumbers.CreatePrimeList(myList);
+            Assert.Equal(3, PrimeNumbers.GetPrime(myList, choice));
+
+        }
+        [Fact]
+        public void Test3()
+        {
+            int choice = 3;
+            ArrayList myList = new ArrayList();
+            PrimeNumbers.CreatePrimeList(myList);
+            Assert.Equal(5, PrimeNumbers.GetPrime(myList, choice));
+
+        }
+        [Fact]
+        public void Test4()
+        {
+            int choice = 4;
+            ArrayList myList = new ArrayList();
+            PrimeNumbers.CreatePrimeList(myList);
+            Assert.Equal(7, PrimeNumbers.GetPrime(myList, choice));
+
+        }
+        [Fact]
+        public void Test5()
+        {
+            int choice = 5;
+            ArrayList myList = new ArrayList();
+            PrimeNumbers.CreatePrimeList(myList);
+            Assert.Equal(11, PrimeNumbers.GetPrime(myList, choice));
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace PrimeLab
+{
+
+    public class PrimeNumbers 
+    {
+            public static void CreatePrimeList(ArrayList list)
+            {
+                int range = 100000;
+                var numbers = new BitArray(range, true);
+
+                for (int i = 2; i < range; i++)
+                    if (numbers[i])
+                    {
+                        for (int j = i * 2; j < range; j += i)
+                            numbers[j] = false;
+                    }
+
+                int primeCount = 0;
+                for (int i = 2; i < range; i++)
+                    if (numbers[i])
+                    {
+                        primeCount++;
+                        list.Add(i);
+                    }
+            }
+
+            public static int GetPrime(ArrayList list, int choice)
+            {
+            return (int)list[choice-1];
+            }
+        }
+
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList myList = new ArrayList();
+            PrimeNumbers.CreatePrimeList(myList);
+
+            bool isRunning = true;
+            while (isRunning)
+            {
+                Console.Clear();
+                Console.WriteLine("Which prime number are you looking for?");
+                bool isValid = int.TryParse(Console.ReadLine(), out int choice);
+                while (!isValid || choice < 1)
+                {
+                    Console.WriteLine("Please enter a valid number greater than 0.");
+                    isValid = int.TryParse(Console.ReadLine(), out choice);
+                }
+                Console.WriteLine($"The {choice} prime number is {PrimeNumbers.GetPrime(myList, choice)}");
+            }
+
+
+
+            
+
+            
+        }
+
+        
+    }
+}
